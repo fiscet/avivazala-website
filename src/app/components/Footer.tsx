64941a6c4046f69ffaf95f3247d442321ee4a19e
Avivazala Admin <admin@avivazala.hu>
2024-07-03 15:06:36 +0200

    Footer copyright translation

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -1,10 +1,13 @@
 import { Link, Locale } from '@lib/i18n';
 import { loadFooterMenu } from '@sanityConf/lib/fetchers';
 import { SanityMenuBuilder } from 'helpers/SanityMenuBuilder';
+import { getTranslations } from 'next-intl/server';
 
 export default async function Footer({ locale }: { locale: Locale }) {
   const currentDate = new Date();
   const currentYear = currentDate.getFullYear();
+
+  const t = await getTranslations('Global');
 
   const menuBuilder = new SanityMenuBuilder({
     locale,
@@ -16,7 +19,9 @@ export default async function Footer({ locale }: { locale: Locale }) {
   return (
     <footer className="bg-base-200 text-base-content p-4">
       <div className="container mx-auto text-center flex flex-col lg:flex-row lg:justify-between">
-        <div>© Minden jog fenntartva {currentYear}.</div>
+        <div>
+          {t('all-rights-reserved')} {currentYear}.
+        </div>
         <div className="flex flex-col lg:flex-row space-x-4 mt-2">
           {menu.items.map((menuItem) => {
             return (
